2e5b1efdd46af53b1810328b277b4bbf432e2d8a
Ken Hu <silvesthu@gmail.com>
2025-11-02 05:55:24 +0900

    Update noise texture

--- Source/Cloud.h
+++ Source/Cloud.h
@@ -65,10 +65,10 @@ public:
 		std::span<Shader> mShaders				= std::span<Shader>(&mShapeNoiseShader, &mSentinelShader);
 		
 		// Textures
-		Texture mShapeNoise3DTexture			= Texture().Width(128).Height(128).Depth(128).Format(DXGI_FORMAT_R8G8B8A8_UNORM).
-															UAVIndex(ViewDescriptorIndex::CloudShapeNoise3DUAV).SRVIndex(ViewDescriptorIndex::CloudShapeNoise3DSRV).SRVFormat(DXGI_FORMAT_R8G8B8A8_UNORM_SRGB).Name("Cloud.ShapeNoise");
-		Texture mErosionNoise3DTexture			= Texture().Width(32).Height(32).Depth(32).Format(DXGI_FORMAT_R8G8B8A8_UNORM).
-															UAVIndex(ViewDescriptorIndex::CloudErosionNoise3DUAV).SRVIndex(ViewDescriptorIndex::CloudErosionNoise3DSRV).SRVFormat(DXGI_FORMAT_R8G8B8A8_UNORM_SRGB).Name("Cloud.ErosionNoise");
+		Texture mShapeNoise3DTexture			= Texture().Width(128).Height(128).Depth(128).Format(DXGI_FORMAT_R8_UNORM).
+															UAVIndex(ViewDescriptorIndex::CloudShapeNoise3DUAV).SRVIndex(ViewDescriptorIndex::CloudShapeNoise3DSRV).Name("Cloud.ShapeNoise");
+		Texture mErosionNoise3DTexture			= Texture().Width(32).Height(32).Depth(32).Format(DXGI_FORMAT_R8_UNORM).
+															UAVIndex(ViewDescriptorIndex::CloudErosionNoise3DUAV).SRVIndex(ViewDescriptorIndex::CloudErosionNoise3DSRV).Name("Cloud.ErosionNoise");
 
 		Texture mShapeNoise2DTexture			= Texture().Width(16384).Height(128).Format(DXGI_FORMAT_R8G8B8A8_UNORM).
 															UAVIndex(ViewDescriptorIndex::CloudShapeNoise2DUAV).SRVIndex(ViewDescriptorIndex::CloudShapeNoise2DSRV).Name("Cloud.ShapeNoise.Input").Path(L"Asset/TileableVolumeNoise/noiseShapePacked.tga");
