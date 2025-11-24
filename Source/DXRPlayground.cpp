39f0407fb84d099b0ddfd83469d1c85531279479
Ken Hu <silvesthu@gmail.com>
2025-11-25 04:39:13 +0900

    Update D3D12 to 1.618.3, dxc to v1.8.2505.1

--- Source/DXRPlayground.cpp
+++ Source/DXRPlayground.cpp
@@ -18,7 +18,7 @@
 
 #include "wincodec.h" // GUID_ContainerFormatPng
 
-extern "C" { __declspec(dllexport) extern const UINT			D3D12SDKVersion = 614; }
+extern "C" { __declspec(dllexport) extern const UINT			D3D12SDKVersion = 618; }
 extern "C" { __declspec(dllexport) extern const char8_t*		D3D12SDKPath = u8".\\D3D12\\"; }
 
 #define DX12_ENABLE_DEBUG_LAYER			(1)
