ceb9e8768123bad50066eea50cf477582d3cac59
Ken Hu <silvesthu@gmail.com>
2026-07-06 19:28:14 +0900

    Add tinybvh

--- Source/Thirdparty/Thirdparty.cpp
+++ Source/Thirdparty/Thirdparty.cpp
@@ -12,3 +12,16 @@
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "Thirdparty/tinyobjloader/tiny_obj_loader.h"
+
+#ifdef _MSC_VER
+// C4100: tinybvh keeps currently-unused extension parameters in public/internal APIs.
+// C4189: tinybvh leaves debug/SAH temporaries live in code paths disabled by macros.
+// C4458: tinybvh uses parameter names that intentionally mirror member names.
+#pragma warning(push)
+#pragma warning(disable: 4100 4189 4458)
+#endif
+#define TINYBVH_IMPLEMENTATION
+#include "tiny_bvh.h"
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
